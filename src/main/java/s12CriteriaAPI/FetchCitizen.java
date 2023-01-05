70fea8cb438a96c10b806cdf07af0d68657c6a2b
osmandgn <oskan001@gmail.com>
2023-01-05 03:57:49 +0300

    V2

---
+++ src/main/java/s12CriteriaAPI/FetchCitizen.java
@@ -0,0 +1,49 @@
+package s12CriteriaAPI;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class FetchCitizen {
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Citizen.class);
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+
+
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<Citizen> criteriaQuery = criteriaBuilder.createQuery(Citizen.class);
+        Root<Citizen> root = criteriaQuery.from(Citizen.class);
+
+
+//        criteriaQuery.select(root);
+//        Query<Citizen> query =  session.createQuery(criteriaQuery);
+//        List<Citizen> citizenList = query.getResultList();
+//        citizenList.forEach(System.out::println);
+
+        // Yaşı 60'dan büyük olanları getir.
+
+        criteriaQuery.select(root).where(criteriaBuilder.greaterThan(root.get("age"), 60));
+        Query<Citizen> query2 = session.createQuery(criteriaQuery);
+        List<Citizen> citizenList1 = query2.getResultList();
+        citizenList1.forEach(System.out::println);
+
+
+
+        tx.commit();
+        session.close();
+        sf.close();
+
+
+    }
+}
--- src/main/java/s12CriteriaAPI/SaveCitizen.java
+++ src/main/java/s12CriteriaAPI/SaveCitizen.java
@@ -1,4 +1,35 @@
 package s12CriteriaAPI;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Random;
+
 public class SaveCitizen {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Citizen.class);
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        for (int i = 0; i < 20; i++) {
+            Citizen citizen = new Citizen();
+            citizen.setName("Citizen " + i);
+            citizen.setAge(random.nextInt(100));
+            session.save(citizen);
+        }
+
+        
+        tx.commit();
+        session.close();
+        sf.close();
+
+
+
+    }
 }
